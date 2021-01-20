ab3654d8304b3b0f727089ca4425182c1d30e82f
Ubuntu <ubuntu@ip-172-31-28-70.us-east-2.compute.internal>
2021-01-20 20:32:21 +0000

    initial commit

---
+++ admin/js/products.js
@@ -0,0 +1,114 @@
+/*global $*/
+
+// READ recods on page load
+$(document).ready(function () {
+    readRecords(); // calling function
+});
+
+// READ records
+function readRecords() {
+    $.get("/products/", {}, function (data, status) {
+        data.forEach(function(value) {
+            var row = '<tr id="row_id_'+ value.id +'">'
+            			+ displayColumns(value)
+        				+ '</tr>';
+            $('#articles').append(row);
+        });
+    });
+}
+
+function displayColumns(value) {
+    return 	'<td>'+value.id+'</td>'
+            + '<td class="category_id">'+ (value.category ? value.category.name : value.category_id) +'</td>'
+            + '<td class="name">'+value.name+'</td>'
+			+ '<td class="description">'+value.description+'</td>'
+			+ '<td align="center">'
+			+	'<button onclick="viewRecord('+ value.id +')" class="btn btn-edit">Actualizare</button>'
+			+ '</td>'
+			+ '<td align="center">'
+			+	'<button onclick="deleteRecord('+ value.id +')" class="btn btn-danger">Stergere</button>'
+			+ '</td>';
+}
+
+function addRecord() {
+    $('#id').val('');
+    $('#category_id').val('');
+    $('#name').val('');
+    $('#description').val('');
+    
+    $('#myModalLabel').html('Adauga un nou produs');
+}
+
+function viewRecord(id) {
+    var url = "/products/" + id;
+    
+    $.get(url, {}, function (data, status) {
+        //bind the values to the form fields
+        $('#category_id').val(data.category_id);
+        $('#name').val(data.name);
+        $('#description').val(data.description);
+        $('#price').val(data.price);
+        $('#id').val(id);
+        $('#myModalLabel').html('Actualizeaza un produs');
+        
+        $('#add_new_record_modal').modal('show');
+    });
+}
+
+function saveRecord() {
+    //get data from the html form
+    var formData = $('#record_form').serializeObject();
+    
+    //decide if it's an edit or create
+    if(formData.id) {
+        updateRecord(formData);
+    } else {
+        createRecord(formData);
+    }
+}
+
+function createRecord(formData) {
+    $.ajax({
+        url: '/products/',
+        type: 'POST',
+        accepts: {
+            json: 'application/json'
+        },
+        data: formData,
+        success: function(data) {
+            $('#add_new_record_modal').modal('hide');
+            
+            var row = '<tr id="row_id_'+ data.id +'">'
+            			+ displayColumns(data)
+        				+ '</tr>';
+            $('#articles').append(row);
+        } 
+    });
+}
+
+function updateRecord(formData) {
+    $.ajax({
+        url: '/products/'+formData.id,
+        type: 'PUT',
+        accepts: {
+            json: 'application/json'
+        },
+        data: formData,
+        success: function(data) {
+            $('#row_id_'+formData.id+'>td.category_id').html(formData.category_id);
+            $('#row_id_'+formData.id+'>td.name').html(formData.name);
+            $('#row_id_'+formData.id+'>td.description').html(formData.description);
+            $('#add_new_record_modal').modal('hide');
+        } 
+    });
+}
+
+function deleteRecord(id) {
+    $.ajax({
+        url: '/products/'+id,
+        type: 'DELETE',
+        success: function(data) {
+            $('#row_id_'+id).remove();
+        }
+    });
+}
---
+++ server.js
@@ -0,0 +1,227 @@
+var express = require("express")
+var Sequelize = require("sequelize")
+
+//connect to mysql database
+//baza de date, username, password
+var sequelize = new Sequelize('catalog', 'username', 'password', {
+    dialect:'mysql',
+    host:'127.0.0.1'
+})
+
+sequelize.authenticate().then(function(){
+    console.log('Success')
+}).catch( function(err) {
+    console.log(err)
+})
+
+//define a new Model
+var Categories = sequelize.define('categories', {
+    name: Sequelize.STRING,
+    description: Sequelize.STRING
+})
+
+var Products = sequelize.define('products', {
+    name: Sequelize.STRING,
+    category_id: Sequelize.INTEGER,
+    description: Sequelize.STRING,
+    price: Sequelize.INTEGER,
+    image: Sequelize.STRING
+})
+
+var Reviews = sequelize.define('reviews', {
+    product_id: Sequelize.INTEGER,
+    name: Sequelize.STRING,
+    content: Sequelize.STRING,
+    score: Sequelize.INTEGER
+})
+
+Products.belongsTo(Categories, {foreignKey: 'category_id', targetKey: 'id'})
+Products.hasMany(Reviews, {foreignKey: 'product_id'});
+
+var app = express()
+
+//access static files
+app.use(express.static('public'))
+app.use('/admin', express.static('admin'))
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
+app.get('/createdb', (request, response) => {
+    sequelize.sync({force: true}).then(() => {
+        response.status(200).send('tables created')
+    }).catch((err) => {
+        response.status(500).send('could not create tables')
+    })
+})
+
+app.get('/createdata', (req, res) => {
+    //TODO add some test data here
+})
+
+async function getCategories(request, response) {
+    try {
+        let categories = await Categories.findAll();
+        response.status(200).json(categories)
+    } catch(err) {
+        response.status(500).send('something bad happened')
+    }
+}
+
+// get a list of categories
+app.get('/categories', getCategories)
+
+// get one category by id
+app.get('/categories/:id', function(request, response) {
+    Categories.findOne({where: {id:request.params.id}}).then(function(category) {
+        if(category) {
+            response.status(200).send(category)
+        } else {
+            response.status(404).send()
+        }
+    })
+})
+
+//create a new category
+app.post('/categories', function(request, response) {
+    Categories.create(request.body).then(function(category) {
+        response.status(201).send(category)
+    })
+})
+
+app.put('/categories/:id', function(request, response) {
+    Categories.findByPk(request.params.id).then(function(category) {
+        if(category) {
+            category.update(request.body).then(function(category){
+                response.status(201).send(category)
+            }).catch(function(error) {
+                response.status(200).send(error)
+            })
+        } else {
+            response.status(404).send('Not found')
+        }
+    })
+})
+
+app.delete('/categories/:id', function(request, response) {
+    Categories.findByPk(request.params.id).then(function(category) {
+        if(category) {
+            category.destroy().then(function(){
+                response.status(204).send()
+            })
+        } else {
+            response.status(404).send('Not found')
+        }
+    })
+})
+
+app.get('/products', function(request, response) {
+    Products.findAll(
+        {
+            include: [{
+                model: Categories,
+                where: { id: Sequelize.col('products.category_id') }
+            }, {
+                model: Reviews,
+                where: { id: Sequelize.col('products.id')},
+                required: false
+            }]
+        }
+        
+        ).then(
+            function(products) {
+                response.status(200).send(products)
+            }
+        )
+})
+
+app.get('/products/:id', function(request, response) {
+    Products.findByPk(request.params.id, {
+            include: [{
+                model: Categories,
+                where: { id: Sequelize.col('products.category_id') }
+            }, {
+                model: Reviews,
+                where: { id: Sequelize.col('products.id')},
+                required: false
+            }]
+        }).then(
+            function(product) {
+                response.status(200).send(product)
+            }
+        )
+})
+
+app.post('/products', function(request, response) {
+    Products.create(request.body).then(function(product) {
+        response.status(201).send(product)
+    })
+})
+
+app.put('/products/:id', function(request, response) {
+    Products.findByPk(request.params.id).then(function(product) {
+        if(product) {
+            product.update(request.body).then(function(product){
+                response.status(201).send(product)
+            }).catch(function(error) {
+                response.status(200).send(error)
+            })
+        } else {
+            response.status(404).send('Not found')
+        }
+    })
+})
+
+app.delete('/products/:id', function(request, response) {
+    Products.findByPk(request.params.id).then(function(product) {
+        if(product) {
+            product.destroy().then(function(){
+                response.status(204).send()
+            })
+        } else {
+            response.status(404).send('Not found')
+        }
+    })
+})
+
+app.get('/categories/:id/products', function(request, response) {
+    Products.findAll({
+            where:{category_id: request.params.id},
+            
+            include: [{
+                model: Categories,
+                where: { id: Sequelize.col('products.category_id') }
+            }, {
+                model: Reviews,
+                where: { id: Sequelize.col('products.id')},
+                required: false
+            }]
+        }
+            ).then(
+            function(products) {
+                response.status(200).send(products)
+            }
+        )
+})
+
+app.get('/reviews', function(request, response) {
+
+})
+
+app.get('/reviews/:id', function(request, response) {
+    
+})
+
+app.post('/reviews', function(request, response) {
+
+})
+
+app.put('/reviews/:id', function(request, response) {
+    
+})
+
+app.delete('/reviews/:id', function(request, response) {
+    
+})
+
+app.listen(8080)
